a1928aed71655c43a744f2d5b4bd7da88f3d601a
Tanguy Pave <tanguy.pave@ynov.com>
2017-10-31 16:53:00 +0100

    Test

--- src/pages/camera/camera.ts
+++ src/pages/camera/camera.ts
@@ -22,15 +22,15 @@ export class CameraPage {
 
   }
 
-  private const options: CameraOptions = {
-  	quality: 100,
-  	destinationType: this.camera.DestinationType.DATA_URL,
-  	encodingType: this.camera.EncodingType.JPEG,
-  	mediaType: this.camera.MediaType.PICTURE
-  }
-
   screen(){
-  	this.camera.getPicture(this.options).then((imageData) => {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+  	this.camera.getPicture(options).then((imageData) => {
       // imageData is either a base64 encoded string or a file URI
       // If it's base64:
     let base64Image = 'data:image/jpeg;base64,' + imageData;
